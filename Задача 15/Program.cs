c78c399ae27298d3fc712411738d5438caa10ddf
vadim <89281779909@mail.ru>
2022-09-09 14:14:44 +0300

    задача 15

--- Задача 15/Program.cs
+++ Задача 15/Program.cs
@@ -3,4 +3,18 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите цифру от 1 до 7, соответствующую дню недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+    if (number > 0 && number < 6)
+    {
+        Console.WriteLine("это рабочий день");
+    }
+    else
+    Console.WriteLine("это выходной день");
+}
+else
+{
+    Console.WriteLine("цифра не соответствует условию");
+}
